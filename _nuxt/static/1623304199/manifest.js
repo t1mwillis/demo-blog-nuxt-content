7e96351303540afce1e6aa02a4f81e624db4f475
Tim Willis <tim@timwill.is>
2021-06-11 21:04:55 -0700

    b2c7979

---
+++ _nuxt/static/1623304199/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fblog\u002Foptimizing-nuxt-vuetify","\u002Fblog\u002Ftag\u002Fjavascript","\u002Fblog\u002Ftag\u002Fvuetify","\u002Fblog\u002Ftag\u002Fnuxtjs","\u002Fblog\u002Fauthor\u002FTim"]})
